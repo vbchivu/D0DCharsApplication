6c8c9ffe128e2f7759c299c75fb02cd3247ba591
vchivu14 <chivu.vlad@yahoo.com>
2021-03-05 12:48:13 +0100

    "Second commit + characteristics"

--- src/main/java/com/dodgame/dodgame/dao/IMonsterDAO.java
+++ src/main/java/com/dodgame/dodgame/dao/IMonsterDAO.java
@@ -1,13 +1,9 @@
 package com.dodgame.dodgame.dao;
 
 import com.dodgame.dodgame.dto.MonsterDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface IMonsterDAO {
-
     List<MonsterDTO> fetchManual(String endpoint) throws Exception;
 }
